bebc1ef4503c19c1ac36da53423609a5103e57a1
Daniel Blankenship <blankenshipdanielj@gmail.com>
2025-10-19 21:51:11 -0500

    fix(cli): remove master sword stub

--- repos/snes-modder/src/index.ts
+++ repos/snes-modder/src/index.ts
@@ -4,7 +4,6 @@
  */
 
 export { ROMHandler, ROMValidationError, ROMModificationError } from './lib/ROMHandler';
-export { MasterSwordMod } from './mods/MasterSwordMod';
 export * from './types/snes.types';
 
 // CLI support
@@ -12,13 +11,8 @@ if (import.meta.url === `file://${process.argv[1]}`) {
   const [,, command, ...args] = process.argv;
   
   switch(command) {
-    case 'master-sword':
-      import('./mods/MasterSwordMod').then(({ MasterSwordMod }) => {
-        MasterSwordMod.fromCLI(args[0], args[1]);
-      });
-      break;
     default:
       // SNES Modder - TypeScript Edition usage information
-      // Available commands: master-sword mod, development server, build, test
+      // Available commands: development server, build, test
   }
 }
